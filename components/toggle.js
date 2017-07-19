41c6b51658daa84073961204b8ade5cca50d2ce4
Prayash Thapa <prayasht@gmail.com>
2017-07-19 07:51:00 -0600

    Updates

---
+++ components/toggle.js
@@ -0,0 +1,46 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
+AFRAME.registerComponent('toggle', {
+  schema: {
+    on: { type: 'string' },
+    dur: { type: 'number', default: 300 }
+  },
+
+  init: function() {
+    var data = this.data
+    var el = this.el
+
+    el.addEventListener(data.on, function() {
+      console.log('HELLO')
+    })
+  },
+
+  /**
+   * Setup fade-in + fade-out.
+   */
+  computeNewState: function() {
+    var data = this.data
+    var targetEl = this.data.target
+    alert('COMPUTE NEW STATE')
+
+    // Only set up once.
+    // if (targetEl.dataset.setImageFadeSetup) {
+    // return
+    // }
+    // targetEl.dataset.setImageFadeSetup = true
+
+    // Create animation.
+    // targetEl.setAttribute('animation__fade', {
+    //   property: 'material.color',
+    //   startEvents: 'set-image-fade',
+    //   dir: 'alternate',
+    //   dur: data.dur,
+    //   from: '#FFF',
+    //   to: '#000'
+    // })
+  }
+})
