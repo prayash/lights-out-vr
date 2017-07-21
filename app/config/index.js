fa8c82f6e120c3389b2e4c06451f3d690664d11c
Prayash Thapa <prayasht@gmail.com>
2017-07-20 18:25:00 -0600

    publish: Edit path
    generated from commit 00da4cf1a9f2dc5cfae71ff3f87a8078f85d43f2

--- app/config/index.js
+++
@@ -1,5 +0,0 @@
-export default {
-  X_OFFSET: -1,
-  Y_OFFSET: 2,
-  SCALE: 1.5
-}
--- app/initialize.js
+++
@@ -1,6 +0,0 @@
-import { h, render } from 'preact'
-import App from './components/App'
-
-document.addEventListener('DOMContentLoaded', () => {
-  render(<App />, document.querySelector('#app'))
-})
